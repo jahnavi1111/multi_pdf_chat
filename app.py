99294bcf907f93bcf7d78f050eb58e175b51cde4
Jahnavi Juluri <jahnavijuluri06@gmail.com>
2025-09-15 20:03:46 -0500

    update to langchain_community to resolve warnings

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st #for GUI
 from dotenv import load_dotenv #load keys
 from PyPDF2 import PdfReader #for handling pdfs
 from langchain.text_splitter import CharacterTextSplitter #for text splitting
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+from langchain_community.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
     text = ""
